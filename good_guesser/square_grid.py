c8ecfead352eba00c7c41feaaef2e12edf2311d2
catsocks <3389801+catsocks@users.noreply.github.com>
2022-05-22 21:59:11 -0300

    Fix square_grid

--- good_guesser/square_grid.py
+++ good_guesser/square_grid.py
@@ -3,18 +3,19 @@ from math import cos, pi, sin
 from random import randint, random
 
 
-# Return a square grid with random rotated squares plotted on it.
-def square_grid(width=50, height=30, res=20):
-    grid = [[" " for _ in range(width)] for _ in range(height)]
+def square_grid():
+    w, h, res = 50, 30, 20
+    bmp = [[" " for _ in range(w)] for _ in range(h)]
     for _ in range(randint(0, 10) + 1):
         size = randint(0, 5) + 2
-        x = (1.5 * size) + randint(0, width - (3 * size))
-        y = (1.5 * size) + randint(0, height - (3 * size))
+        x = (1.5 * size) + int(random() * (w - (3 * size)))
+        y = (1.5 * size) + int(random() * (h - (3 * size)))
         rot = pi * 2 * random()
         dx, dy = sin(rot), cos(rot)
         idx, idy = -dy, dx
         axes_range = [size * (1 / res) * i for i in range(-res, res)]
 
         for xx, yy in product(axes_range, axes_range):
-            grid[int(y + (dy * xx) + (idy * yy))][int(x + (dx * xx) + (idx * yy))] = "*"
-    return grid
+            row, col = int(y + (dy * xx) + (idy * yy)), int(x + (dx * xx) + (idx * yy))
+            bmp[row][col] = "*"
+    return bmp
